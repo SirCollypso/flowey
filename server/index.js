b7de277e44283af24f2daa27c5a45ab0c1c7af00
Amin Jalilov <aminkapkan444@gmail.com>
2024-12-18 19:38:27 +0900

    chore: add speech lock

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ import OpenAI from 'openai';
 import express from 'express';
 import os from 'os';
 import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
 import 'dotenv/config';
 
 const PORT = 8080;
+
+const ASSETS_DIR = path.resolve('assets');
+const audioFile = path.join(ASSETS_DIR, 'latest_speech.mp3');
 
 // Set up an Express app and enable JSON parsing
 const app = express();
@@ -19,17 +24,15 @@ const ASSISTANT_NAME = 'Flowey';
 // Retrieve the assistant or create a new one
 const assistant = await getAssistant(ASSISTANT_NAME);
 
-// Load the existing thread ID from environment variables or create a new thread if none
-let thread_id = process.env.THREAD_ID;
-if (!thread_id) {
-    thread_id = (await createThread()).id;
-    fs.appendFileSync('.env', `\nTHREAD_ID=${thread_id}`);
-}
+// Create a new thread if none
+const thread_id = (await createThread()).id;
 
 // Start the server
 app.listen(PORT, () => {
     console.log(os.networkInterfaces());
 });
+
+let currently_speaking = false;
 
 // Keep track of the last touch sensor state
 let last_touch = 0;
@@ -44,18 +47,30 @@ app.post('/sensors', async (req, res) => {
     
     // Only invoke the assistant when the touch sensor changes from 0 to 1
     if (touch == 1 && last_touch == 0) {
-        invokeAssistant(
-            assistant,
-            thread_id,
-            'Touch sensor is triggered.',
-            'You must pretend that you are a lovely flower. You are connected to sensors attached to a real plant. You will get updates each time some sensor is triggered.'
-        )
-        .then((response) => {
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log('An error occurred while processing your request: ' + (error.response ? error.response.data : error.message));
-        });
+        if (!currently_speaking) {
+            invokeAssistant(
+                assistant,
+                thread_id,
+                'Touch sensor is triggered.',
+                'You must pretend that you are a lovely flower. You are connected to sensors attached to a real plant. You will get updates each time some sensor is triggered.'
+            )
+            .then((response) => {
+                console.log(response.message);
+                currently_speaking = true;
+                textToSpeech(response.message, audioFile)
+                .then(() => {
+                    exec(`cvlc --play-and-exit ${audioFile}`, (error, _, __) => {
+                        currently_speaking = false;
+                        if (error !== null) {
+                            console.log(`exec error: ${error}`);
+                        }
+                    });
+                })
+            })
+            .catch((error) => {
+                console.log('An error occurred while processing your request: ' + (error.response ? error.response.data : error.message));
+            });
+        }
     }
     last_touch = touch;
 });
@@ -122,4 +137,14 @@ function invokeAssistant(
             message: answer
         };
     });
+}
+
+async function textToSpeech(text, outputFile) {
+    const response = await openai.audio.speech.create({
+        model: 'tts-1',
+        voice: 'onyx',
+        input: text,
+    });
+    const buffer = Buffer.from(await response.arrayBuffer());
+    fs.writeFileSync(outputFile, buffer);
 }
